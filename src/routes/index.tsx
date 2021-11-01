efd8c40a7fb73785a3ba693e0665fa1ef17b4100
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-01 19:43:25 -0300

    :hammer: Initial config with eslint and prettier

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Route, Switch } from "react-router-dom";
+
+export function Routes() {
+    return (
+        <>
+            <Switch>{/* <Route path="/" exact component={Home} /> */}</Switch>
+        </>
+    );
+}
