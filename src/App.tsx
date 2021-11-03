31813184862cb5db060177ebe3d536d680d23176
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-03 11:42:21 -0300

    :wrench: feat: routes and globalStyle added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { Routes } from "./routes";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
-function App() {
+const App: React.FC = () => {
     return (
-        <BrowserRouter>
-            <Routes />
-            {/* <GlobalStyle /> */}
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <Routes />
+                <GlobalStyle />
+            </BrowserRouter>
+        </>
     );
-}
+};
 
 export default App;
