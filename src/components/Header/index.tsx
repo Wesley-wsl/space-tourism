df75b2be6bb76f353d2e54e77bfe8d6ec4904932
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-05 16:19:44 -0300

    :wrench: feat: effect active in header created

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 import Logo from "../../assets/shared/logo.svg";
 import { Container } from "./styles";
 
 const Header: React.FC = () => {
+    const location = useLocation();
+    useEffect(() => {
+        const li = document.querySelectorAll("li[data-li]");
+        switch (location.pathname) {
+            case "/":
+                li[0].classList.add("activeNav");
+                break;
+            case "/destination":
+                li[1].classList.add("activeNav");
+                break;
+            case "/crew":
+                li[2].classList.add("activeNav");
+                break;
+            case "/technology":
+                li[3].classList.add("activeNav");
+                break;
+            default:
+                break;
+        }
+    });
+
     return (
         <Container>
             <div className="logo">
@@ -14,25 +36,25 @@ const Header: React.FC = () => {
 
             <nav>
                 <ul>
-                    <li>
-                        <a href="#Logo">
+                    <li data-li>
+                        <Link to="/">
                             <span>00</span> HOME
-                        </a>
+                        </Link>
                     </li>
-                    <li>
-                        <a href="#.">
+                    <li data-li>
+                        <Link to="destination">
                             <span>01</span> DESTINATION
-                        </a>
+                        </Link>
                     </li>
-                    <li>
-                        <a href="#.">
+                    <li data-li>
+                        <Link to="/crew">
                             <span>02</span> CREW
-                        </a>
+                        </Link>
                     </li>
-                    <li>
-                        <a href="#.">
+                    <li data-li>
+                        <Link to="/technology">
                             <span>03</span> TECHNOLOGY
-                        </a>
+                        </Link>
                     </li>
                 </ul>
             </nav>
