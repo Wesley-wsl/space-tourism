f60883cee29f31021ec59dcbc5af1f0cb3932a3d
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-05 16:21:04 -0300

    :art: style: background container created

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,4 +1,6 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+import { backgroundType } from "../@types";
 
 export const GlobalStyle = createGlobalStyle`
 
@@ -18,4 +20,14 @@ a {
     text-decoration: none;
     color: #fff;
 }
+`;
+
+export const BackgroundContainer = styled.div`
+    background-image: url(${({ background }: backgroundType) => background});
+    background-size: cover;
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    filter: saturate(250%);
 `;
