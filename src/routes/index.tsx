bbcd7fa972fb636fe40fe03f3a7bf371377cf173
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-06 00:56:49 -0300

    :wrench: feat: routes for crew and technology created

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,9 @@
 import { Route, Switch } from "react-router-dom";
 
+import Crew from "../pages/Crew";
 import Destination from "../pages/Destination";
 import Home from "../pages/Home";
+import Technology from "../pages/Technology";
 
 export const Routes: React.FC = () => {
     return (
@@ -9,6 +11,8 @@ export const Routes: React.FC = () => {
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/destination" exact component={Destination} />
+                <Route path="/crew" exact component={Crew} />
+                <Route path="/technology" exact component={Technology} />
             </Switch>
         </>
     );
