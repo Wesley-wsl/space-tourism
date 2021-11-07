e49807c35eb2fcb8da5c8a12bd84eca84615f252
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-07 12:28:17 -0300

    :hammer: build: types for application created

---
+++ src/@types/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/ban-types */
+
+export interface IBackground {
+    backgroundDesktop: string;
+    backgroundTablet: string;
+    backgroundMobile: string;
+}
+
+export interface IMenuMobile {
+    isOpen: boolean;
+}
